719e9fd8b77352c2ba5e4c85cebb6152bdeacc3c
Techlab <flutterhub.bd@gmail.com>
2024-05-11 03:27:30 -0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import "package:webview_universal/webview_universal.dart";
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
@@ -16,31 +17,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  WebViewController webViewController = WebViewController();
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    webViewController.init(
-      context: context,
-      setState: setState,
-      uri: Uri.parse("https://teerwallet.com/"),
-    );
+    _controller = WebViewController()
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (request) async {
+            final urlString = request.url.toString();
+            final uri = Uri.parse(urlString);
+            if (await canLaunchUrl(uri)) {
+              await launchUrl(uri, mode: LaunchMode.externalApplication);
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://teerwallet.com/'));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   leading: FloatingActionButton(
-      //     onPressed: () {
-      //       webViewController.goBackSync();
-      //     },
-      //     child: Icon(Icons.abc),
-      //   ),
-      // ),
-      body: WebView(
-        controller: webViewController,
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack();
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Teer Wallet",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFFEE4248),
+        ),
+        body: WebViewWidget(
+          controller: _controller,
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import url_launcher_macos
 import webview_universal
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
 }
