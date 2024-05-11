c3bf5e6a4dd564a961f223e977b87389bab9c27b
Techlab <flutterhub.bd@gmail.com>
2024-05-11 00:52:50 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import "package:webview_universal/webview_universal.dart";
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  WebViewController webViewController = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    webViewController.init(
+      context: context,
+      setState: setState,
+      uri: Uri.parse("https://teerwallet.com/"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   leading: FloatingActionButton(
+      //     onPressed: () {
+      //       webViewController.goBackSync();
+      //     },
+      //     child: Icon(Icons.abc),
+      //   ),
+      // ),
+      body: WebView(
+        controller: webViewController,
+      ),
+    );
+  }
+}
